f3f4af6c97a0aba2ce32148b90d9fcc904862731
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-07-18 10:51:40 +0300

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+
+
+import React from 'react'
+
+function Keypad() {
+
+  function change() {
+    console.log("Entering password...")
+  }
+
+  return (
+    <input type="password" onChange={change} />
+  )
+}
+
+export default Keypad;
